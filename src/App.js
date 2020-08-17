ffa54aa5dd9e506baf9a95ba559a137e7ffc8e5a
Mateusz Kotwiński <mkotwinski98@gmail.com>
2020-08-17 12:15:48 +0200

    Add router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 
 import PokemonList from './components/pokemonList';
 
@@ -6,9 +7,9 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        Hello
-      </div>
+      <Router>
+        <Route path="/" exact component={PokemonList} />
+      </Router>
     );
   }
 }
