03285808b63a21bf2ae454843b19ed7feb09a087
Mateusz Kotwiński <mkotwinski98@gmail.com>
2020-08-25 15:26:39 +0200

    Add logic to render spinner

--- src/components/pokemon.js
+++ src/components/pokemon.js
@@ -1,40 +1,54 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+
+import Spinner from '../components/features/Spinner/Spinner';
 import '../styles/pokemon.scss';
 
 class Pokemon extends Component {
     state = {
         name: this.props.pokemonName,
         url: this.props.pokemonUrl,
+        pokemonIndex: '',
+        imageUrl: '',
+        isLoading: true,
         height: '',
         weight: '',
     }
 
     componentDidMount() {
         axios.get(this.state.url)
-            .then(response => {
-                this.setState({height: response.data.height, weight: response.data.weight})
-                console.log(response)
-            })
-            .catch((error) => {
+        .then(response => {
+            this.setState({height: response.data.height, weight: response.data.weight})
+        })
+        .catch((error) => {
             console.log(error);
-            })
+        })
+
+        const pokemonIndex = this.state.url.split('/')[this.state.url.split('/').length - 2];
+        const imageUrl = `https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${pokemonIndex}.png?raw=true`;
+        this.setState({pokemonIndex: pokemonIndex, imageUrl: imageUrl});
+    }
+
+    componentWillMount() {
+        setTimeout(() => this.setState({isLoading: false}), 1000)
     }
 
     capitalizeFirstLetter(string) {
         return string.charAt(0).toUpperCase() + string.slice(1);
     }
 
-    render() {
-        const pokemonIndex = this.state.url.split('/')[this.state.url.split('/').length - 2];
+    
 
-        const imageUrl = `https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${pokemonIndex}.png?raw=true`;
+    render() {
+        // const pokemonIndex = this.state.url.split('/')[this.state.url.split('/').length - 2];
+
+        // const imageUrl = `https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${this.state.pokemonIndex}.png?raw=true`;
 
         return (
             <div className="pokemon-container">
-                <h1 className="pokemon-title">{pokemonIndex}. {this.capitalizeFirstLetter(this.state.name)}</h1>
+                <h1 className="pokemon-title">{this.state.pokemonIndex}. {this.capitalizeFirstLetter(this.state.name)}</h1>
                 <div className="pokemon-img">
-                    <img src={imageUrl} />
+                    {this.state.isLoading === true ? <Spinner /> : <img src={this.state.imageUrl} />}
                 </div>
                 <div className="pokemon-info-container">
                     <p className="pokemon-info">Height: {this.state.height}</p>
